8fef86e442d217060620be4c9c311cb709d840fe
mollerhoj <mollerhoj3@gmail.com>
2013-08-29 18:09:17 +0200

    first 3 tests pass

--- stable_marriage.rb
+++ stable_marriage.rb
@@ -1,7 +1,18 @@
-
-#PROGRAM
-
-def compute(people,pref)
-  return nil
+def compute(men,women)
+  txt = ''
+  men.each do |m|
+    txt += m.name + ' -- ' + m.pref.first.name + "\n"
+  end
+  txt
 end
 
+class Human
+  attr_reader :name
+  attr_reader :pref
+
+  def initialize args
+    args.each do |k,v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
+  end
+end
--- test_stable_marriage.rb
+++ test_stable_marriage.rb
@@ -3,16 +3,34 @@
 require "test/unit"
 require "./stable_marriage"
 
-#TEST
-
 class TestStableMarriages < Test::Unit::TestCase
 
   def test_no_people_no_marriages
-    assert compute(nil,nil)==nil
+    assert_equal "", compute([],[])
   end
 
   def test_two_people_one_marriage
-    assert compute(2,
+    women = [Human.new(name: 'Monica')]
+    men = [Human.new(name: 'Joey',pref: women)]
+    assert_equal "Joey -- Monica\n", compute(men,women)
+
+    women = [Human.new(name: 'Racheal')]
+    men = [Human.new(name: 'Ross',pref: women)]
+    assert_equal "Ross -- Racheal\n", compute(men,women)
+  end
+
+  def test_four_people_two_marriages
+
+    men = []
+    women = []
+
+    monica = Human.new(name: 'Monica')
+    racheal = Human.new(name: 'Racheal')
+
+    men << Human.new(name: 'Joey', pref: [monica,racheal])
+    men << Human.new(name: 'Ross', pref: [racheal,monica])
+
+    assert_equal "Joey -- Monica\nRoss -- Racheal\n", compute(men,women)
   end
 
 end
